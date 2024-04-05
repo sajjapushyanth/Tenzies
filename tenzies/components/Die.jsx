09799839601a589bb648d0655932834a927397a3
sajjapushyanth <sp7173@srmist.edu.in>
2024-04-05 09:50:31 +0530

    die-value classname added

--- tenzies/components/Die.jsx
+++ tenzies/components/Die.jsx
@@ -5,7 +5,7 @@ export default function Die(props){
          className={`die-face ${props.isHeld?"true":""}`}
          onClick={props.holdDice}
          >
-            <h2>{props.value}</h2>
+            <h2 className="die-value">{props.value}</h2>
         </div>
     )
   
